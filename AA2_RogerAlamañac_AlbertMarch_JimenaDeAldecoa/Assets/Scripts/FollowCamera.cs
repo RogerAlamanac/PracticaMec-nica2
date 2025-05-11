85b0cf6f5262f6152e93f63c8879e32d2dafc357
Albert <albert.march@enti.cat>
2025-05-11 22:27:53 +0200

    Falta comentaris

--- AA2_RogerAlamañac_AlbertMarch_JimenaDeAldecoa/Assets/Scripts/FollowCamera.cs
+++ AA2_RogerAlamañac_AlbertMarch_JimenaDeAldecoa/Assets/Scripts/FollowCamera.cs
@@ -2,24 +2,30 @@ using UnityEngine;
 
 public class FollowCamera: MonoBehaviour
 {
-    public Transform target; 
+    public Transform target;
     public Vector3 offset = new Vector3(0, 3, -6);
     public float smoothSpeed = 0.125f;
     public float rotationSpeed = 5f;
 
-    private float currentYaw = 0f;
+    public float initialYaw = 0f;
+
+    private float currentYaw;
+
+    void Start()
+    {
+        currentYaw = initialYaw;
+    }
 
     void LateUpdate()
     {
         if (target == null) return;
 
-    
-        if (Input.GetMouseButton(1)) // Click dret del ratolí
+        if (Input.GetMouseButton(1))
         {
             float mouseX = Input.GetAxis("Mouse X");
             currentYaw += mouseX * rotationSpeed;
         }
- 
+
         Quaternion rotation = Quaternion.Euler(0, currentYaw, 0);
         Vector3 desiredPosition = target.position + rotation * offset;
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
